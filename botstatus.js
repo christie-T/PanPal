c8cce88d3b7e9031bdfa66b800df36f5bcf3d483
Christie Tang <60620814+christie-T@users.noreply.github.com>
2023-04-02 22:25:26 -0400

    Update botstatus.js

--- botstatus.js
+++ botstatus.js
@@ -1,6 +1,5 @@
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
-// Set a streaming status. If a twitch URL is provided it will show up as a button
 await lib.discord.users['@0.2.0'].me.status.update({
   activity_name: 'The Taco Song', // required
   activity_type: 'LISTENING',
